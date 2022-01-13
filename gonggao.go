e3988474ce73ae4679dce1165d112c276b21cc39
letsfire <249008728@qq.com>
2022-01-13 12:09:25 +0800

    提交

--- gonggao.go
+++ gonggao.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
 type gongGaoRes struct {
@@ -29,42 +28,15 @@ func (d *GongGaoData) PdfUrl() string {
 	return fmt.Sprintf("https://pdf.dfcfw.com/pdf/H2_%s_1.pdf", d.ArtCode)
 }
 
-func (d *GongGaoData) contentUrl() string {
-	api := "https://np-cnotice-pc.eastmoney.com/api/content/ann/rich"
-	return fmt.Sprintf(api+"?client_source=pc&is_rich=1&art_code=%s#%s", d.ArtCode, d.ArtCode)
-}
-
-func (d *GongGaoData) fetchContent() error {
-	return nil
-}
-
 type GongGaoDatas []*GongGaoData
 
-func (ds GongGaoDatas) fetchContent() error {
-	var errs = make(Errors, 0)
-	line := pool.AddLine(func(i interface{}) {
-		errs.add(ds[i.(int)].fetchContent())
-	})
-	for index := range ds {
-		line.Submit(index)
-	}
-	line.Wait() // wait done
-	return errs.first()
-}
-
 // AllGonGao 公告查询
+// market: SHA, SZA, BJA, CYB, KCB
 // fNode: 1.财务报告 2.融资公告 3.风险提示 4.信息变更 5.重大事项 6.资产重组 7.持股变动
-func AllGonGao(begin, end string, fNode ...string) (GongGaoDatas, error) {
-	ds, err := doFetchGongGao(begin, end, fNode)
-	return ds, callWithoutErr(err, ds.fetchContent)
-}
-
-const gongGaoApi = "https://np-anotice-stock.eastmoney.com/api/security/ann"
-
-func doFetchGongGao(begin, end string, fNode []string) (GongGaoDatas, error) {
+func AllGonGao(market, begin, end string, fNode string) (GongGaoDatas, error) {
 	var query = fmt.Sprintf(
 		"?begin_time=%s&end_time=%s&ann_type=%s&f_node=%s",
-		begin, end, "SHA,SZA,BJA,CYB,KCB", strings.Join(fNode, ","),
+		begin, end, market, fNode,
 	)
 	var datass, page = make(GongGaoDatas, 0), 1
 	for {
@@ -76,6 +48,8 @@ func doFetchGongGao(begin, end string, fNode []string) (GongGaoDatas, error) {
 		page++ // next page
 	}
 }
+
+const gongGaoApi = "https://np-anotice-stock.eastmoney.com/api/security/ann"
 
 func doFetchGongGaoPage(query string, pageNo int) (GongGaoDatas, error) {
 	var res = new(gongGaoRes)
