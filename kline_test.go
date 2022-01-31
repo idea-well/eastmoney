c79b9bfafdf3c989b88c26ea076ab2363698164f
letsfire <249008728@qq.com>
2022-01-31 12:44:53 +0800

    tijiao

--- kline_test.go
+++ kline_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestKLine(t *testing.T) {
-	data, err := KLine("300547", 0)
-	fmt.Println(err, len(data), data["20220128"])
-	fmt.Println(KLineDate("300547", 0, "20220128"))
+	//data, err := KLine("300547", 0)
+	//fmt.Println(err, len(data), data["20220128"])
+	fmt.Println(KLineDate("600112", 1, "20220104"))
 }
