e47e85ef1937a48be215ddaea6ed37512f17eba5
letsfire <249008728@qq.com>
2022-02-02 14:03:25 +0800

    tijiao

--- kline.go
+++ kline.go
@@ -17,23 +17,27 @@ type kLineRes struct {
 
 type kLineStrings []string
 
-func (ks kLineStrings) toData(lastPre float64) map[string]*KLineData {
+func (ks kLineStrings) toData(lastPre float64, min bool) map[string]*KLineData {
 	map_ := make(map[string]*KLineData)
 	for _, kStr := range ks {
+		var key string
 		ss := strings.Split(kStr, ",")
-		date := strings.ReplaceAll(ss[0], "-", "")
-		map_[date] = &KLineData{PreClose: lastPre}
-		map_[date].Open = ParseFloat(ss[1])
-		map_[date].Close = ParseFloat(ss[2])
-		map_[date].High = ParseFloat(ss[3])
-		map_[date].Low = ParseFloat(ss[4])
-		map_[date].Volume = ParseInt(ss[5])
-		map_[date].Amount = ParseFloat(ss[6])
-		map_[date].Amplitude = ParseFloat(ss[7])
-		map_[date].Change = ParseFloat(ss[8])
-		map_[date].Turnover = ParseFloat(ss[9])
-		map_[date].AvgPrice = map_[date].Amount / (float64(map_[date].Volume) * 100)
-		lastPre = map_[date].Close
+		if min {
+			key = strings.ReplaceAll(strings.Split(ss[0], " ")[1], ":", "")
+		} else {
+			key = strings.ReplaceAll(ss[0], "-", "")
+		}
+		map_[key] = &KLineData{PreClose: lastPre}
+		map_[key].Open = ParseFloat(ss[1])
+		map_[key].Close = ParseFloat(ss[2])
+		map_[key].High = ParseFloat(ss[3])
+		map_[key].Low = ParseFloat(ss[4])
+		map_[key].Volume = ParseInt(ss[5])
+		map_[key].Amount = ParseFloat(ss[6])
+		map_[key].Amplitude = ParseFloat(ss[7])
+		map_[key].Change = ParseFloat(ss[8])
+		map_[key].Turnover = ParseFloat(ss[9])
+		lastPre = map_[key].Close
 	}
 	return map_
 }
@@ -50,12 +54,25 @@ type KLineData struct {
 	Turnover  float64 `json:"turnover"`  // 换手率
 	Amplitude float64 `json:"amplitude"` // 日振幅
 	PreClose  float64 `json:"pre_close"` // 昨收盘
-	AvgPrice  float64 `json:"avg_price"` // 平均价
 }
 
 const kLineApi = "http://push2his.eastmoney.com/api/qt/stock/kline/get" +
 	"?fields1=f6&fields2=f51,f52,f53,f54,f55,f56,f57,f58,f59,f61" +
-	"&klt=101&fqt=1&beg=%s&end=%s&lmt=1000000&secid=%d.%s"
+	"&klt=%d&fqt=1&beg=%s&end=%s&lmt=1000000&secid=%d.%s"
+
+func MLine(code string, market int) (map[string]*KLineData, error) {
+	var res = new(kLineRes)
+	url := fmt.Sprintf(kLineApi, 1, "0", "20500101", market, code)
+	resp, err := http.Get(url)
+	err = callWithoutErr(err, func() error {
+		defer resp.Body.Close()
+		bts, err := ioutil.ReadAll(resp.Body)
+		return callWithoutErr(err, func() error {
+			return json.Unmarshal(bts, res)
+		})
+	})
+	return res.Data.KLines.toData(res.Data.PreKPrice, true), err
+}
 
 func KLine(code string, market int, date ...string) (map[string]*KLineData, error) {
 	var beg, end string
@@ -66,7 +83,7 @@ func KLine(code string, market int, date ...string) (map[string]*KLineData, erro
 		beg = MinString(date[0], date...)
 		end = MaxString(date[0], date...)
 	}
-	url := fmt.Sprintf(kLineApi, beg, end, market, code)
+	url := fmt.Sprintf(kLineApi, 101, beg, end, market, code)
 	resp, err := http.Get(url)
 	err = callWithoutErr(err, func() error {
 		defer resp.Body.Close()
@@ -75,7 +92,7 @@ func KLine(code string, market int, date ...string) (map[string]*KLineData, erro
 			return json.Unmarshal(bts, res)
 		})
 	})
-	return res.Data.KLines.toData(res.Data.PreKPrice), err
+	return res.Data.KLines.toData(res.Data.PreKPrice, false), err
 }
 
 func KLineDate(code string, market int, date string) (*KLineData, error) {
--- kline_test.go
+++ kline_test.go
@@ -10,3 +10,8 @@ func TestKLine(t *testing.T) {
 	fmt.Println(err, len(data), data["20220128"])
 	fmt.Println(KLineDate("600112", 1, "20220104"))
 }
+
+func TestMLine(t *testing.T) {
+	data, err := MLine("300547", 0)
+	fmt.Println(err, len(data), data["0931"])
+}
